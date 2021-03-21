b4cb0e00469a2e62c5f7c62a8a4140060fa23bcd
davidflores8 <davidflores126@unitec.edu>
2021-03-20 18:59:05 -0600

    Esta es la primer funcion

--- Proyecto.hs
+++ Proyecto.hs
@@ -1,19 +1,37 @@
+{-# OPTIONS_GHC -Wno-deferred-type-errors #-}
+{-# LANGUAGE OverloadedStrings #-}
 import Data.List.Split
 import System.IO
 import Control.Monad
 import Control.Monad.IO.Class
-import Data.IORef
-import Data.ByteString.Search.KarpRabin
-
+import Data.IORef ()
+import Data.ByteString.Search.KarpRabin (indicesOfAny)
 import qualified Data.ByteString as B
+import Data.ByteString.Char8 ()
+import Data.ByteString.Lazy.Char8 (ByteString)
+
+
+
+obtenerCantidad::[(Int,[Int])]->Int
+obtenerCantidad = length
+
 
 main :: IO ()
-
 main = do
-    theFile <- openFile "Texto.txt" ReadMode
-    contents <- hGetContents theFile
-    print(splitOn " " contents)
-    --print(split ' ' contents)
-    hClose theFile
+    file <- B.readFile "Prueba.txt"
+   -- palabras <- getLine 
+    let palabras = ["ingles"]
+    let tuples = indicesOfAny palabras file
+    print "Estas son las tuplas dentro del archivo"
+    print tuples
+    print "Este es el length"
+    print $ length tuples
+    print "Esta es la funcion"
+    print $ obtenerCantidad tuples
+
+
+
+
+
 
 
