306c67f43991cc73de70443707a987e7279e95d3
davidflores8 <davidflores126@unitec.edu>
2021-03-20 20:07:23 -0600

    Empezando el tercer ejercicio

--- Proyecto.hs
+++ Proyecto.hs
@@ -7,6 +7,7 @@ import Control.Monad.IO.Class
 import Data.IORef ()
 import Data.ByteString.Search.KarpRabin (indicesOfAny)
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy.UTF8 as C
 import Data.ByteString.Char8 ()
 import Data.ByteString.Lazy.Char8 (ByteString)
 
@@ -19,13 +20,11 @@ obtenerCantidad = length
 main :: IO ()
 main = do
     file <- B.readFile "Prueba.txt"
-   -- palabras <- getLine 
+    otras <- sequence [getLine, getLine, getLine]
     let palabras = ["ingles"]
     let tuples = indicesOfAny palabras file
     print "Estas son las tuplas dentro del archivo"
     print tuples
-    print "Este es el length"
-    print $ length tuples
     print "Esta es la funcion"
     print $ obtenerCantidad tuples
 
