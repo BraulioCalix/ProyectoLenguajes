5f87f2f9f5ead0765792c000624cfb6b09300ade
davidflores8 <davidflores126@unitec.edu>
2021-03-20 20:18:52 -0600

    Funcion 3 terminada

--- Proyecto.hs
+++ Proyecto.hs
@@ -7,7 +7,6 @@ import Control.Monad.IO.Class
 import Data.IORef ()
 import Data.ByteString.Search.KarpRabin (indicesOfAny)
 import qualified Data.ByteString as B
-import qualified Data.ByteString.Lazy.UTF8 as C
 import Data.ByteString.Char8 ()
 import Data.ByteString.Lazy.Char8 (ByteString)
 
@@ -17,12 +16,13 @@ obtenerCantidad::[(Int,[Int])]->Int
 obtenerCantidad = length
 
 
+
 main :: IO ()
 main = do
     file <- B.readFile "Prueba.txt"
-    otras <- sequence [getLine, getLine, getLine]
-    let palabras = ["ingles"]
-    let tuples = indicesOfAny palabras file
+    print "Ingrese 3 palabras para verificar si estan en el documento"
+    otras <- sequence [B.getLine, B.getLine, B.getLine]
+    let tuples = indicesOfAny otras file
     print "Estas son las tuplas dentro del archivo"
     print tuples
     print "Esta es la funcion"
